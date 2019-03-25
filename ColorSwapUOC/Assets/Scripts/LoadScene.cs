385d92bdc923e21cc919c28142f6d0adb6f09f7c
David Masnou Mayoral <davidmasnou@gmail.com>
2019-03-25 21:15:38 +0100

    Begining project in unity

---
+++ ColorSwapUOC/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void LoadStartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+    }
+}
