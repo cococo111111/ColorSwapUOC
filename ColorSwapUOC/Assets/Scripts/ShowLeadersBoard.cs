8d776d9bc63c70e408c587453a7e6c0b37287bf2
David Masnou Mayoral <davidmasnou@gmail.com>
2019-05-22 21:07:55 +0200

    Menu definitivo, Shop 25%, new Cards

---
+++ ColorSwapUOC/Assets/Scripts/ShowLeadersBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EasyMobile;
+using UnityEngine;
+
+public class ShowLeadersBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameServices.IsInitialized())
+        {
+            GameServices.Init();
+        }
+    }
+
+    public void ShowLeaders()
+    {
+        GameServices.ShowLeaderboardUI();
+    }
+}
